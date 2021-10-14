e4dcbfd140ddbbd26c4f26258e088697be9ecb55
Warlex89 <AlehDybau@protonmail.com>
2021-10-15 01:50:18 +0300

    Добавлено задание 9 из Раздела Algorithmization

---
+++ src/com/company/Algorithmization/Task_9/Task9.java
@@ -0,0 +1,37 @@
+package com.company.Algorithmization.Task_9;
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+import java.util.Arrays;
+
+public class Task9 {
+    public static void main(String[] args) {
+        int n, array[], l;
+        out.println("В массиве целых чисел с количеством элементов n найти наиболее часто встречающееся число. Если таких\n" +
+                "чисел несколько, то определить наименьшее из них.");
+        Scanner sc = new Scanner(in);
+        out.println("Введите размер массива N ");
+        n = sc.nextInt();
+        array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            l = (int) (random() * n);// создаётся случайное действительное число от -n до n для массива
+            array[i] = l;
+        }
+        int count = 0; // подсёт повторений у одного элемента
+        int nmb1 = array[0]; // выводимое число, которое чаще повторяется, но меньше из других
+        int maxcount = 0; // подсчитывает максимальное количество повторений
+        int countSt;// значение массива, которое просчитывается
+        for (int i = 0; i < array.length; i++){
+            countSt = array[i];
+            count = 0;
+            for (int j = 0; j < array.length; j++){
+                count = (countSt == array[j]) ? (++count) : count;// подсчёт количества повторений
+                nmb1 = (count > maxcount) ? countSt : nmb1;// проверка на количеств повторений
+                nmb1 = (count == maxcount) ? min(nmb1,countSt) : nmb1;// проверка на наименьшее число
+                maxcount = (count > maxcount) ? count : maxcount;
+            }
+        }
+        out.println("Искомая переменная = "+nmb1);
+        out.println("Получен новый массив "+ Arrays.toString(array));
+    }
+}

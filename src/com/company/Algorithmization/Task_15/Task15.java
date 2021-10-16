1dfddded699f4f0e78f8c0e365f6c70ce56e4045
Warlex89 <AlehDybau@protonmail.com>
2021-10-16 21:55:49 +0300

    Добавлено задание 15 из Раздела Algorithmization

---
+++ src/com/company/Algorithmization/Task_15/Task15.java
@@ -0,0 +1,34 @@
+package com.company.Algorithmization.Task_15;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Task15 {
+    public static void main(String[] args) {
+        int n, k = 1, array[][];
+        out.println("Сформировать квадратную матрицу порядка n по заданному образцу(n - четное)");
+        Scanner sc = new Scanner(in);
+        out.println("Введите размер матрицы ");
+        n = sc.nextInt();
+        array = new int[n][n];
+        out.println("Получена искомая матрица ");
+        for (int i = 0; i < n; i++) {
+            if(i != 0){
+                for (int j = 0; j < n - i; j++){
+                    array[i][j] = n - (n-k);
+                    out.print(" " + array[i][j] + " ");
+                }
+                for(int j = n - i; j < n; j++){
+                    array[i][j] = 0;
+                    out.print(" " + array[i][j] + " ");
+                }
+            } else {
+                for (int j = 0; j < n; j++) {
+                    array[i][j] = n - (n - k);
+                    out.print(" " + array[i][j] + " ");
+                }
+            }
+            k++;
+            out.println();
+        }
+    }
+}

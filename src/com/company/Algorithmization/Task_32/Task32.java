0df99b8190ce9685571e75b14e57e49201672393
Warlex89 <AlehDybau@protonmail.com>
2021-10-27 20:41:13 +0300

    Добавлены задания 32 и 33

---
+++ src/com/company/Algorithmization/Task_32/Task32.java
@@ -0,0 +1,43 @@
+package com.company.Algorithmization.Task_32;
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+import java.util.Random;
+
+
+public class Task32 {
+    public static void main(String[] args) {
+    Random random = new Random();
+    int n, array[], l, h = 1;
+    boolean st;
+    out.println("Сортировка Шелла");
+    Scanner sc = new Scanner(in);
+    out.println("Введите размер массива N ");
+    n = sc.nextInt();
+    array = new int[n];
+    // Создаём массив действительных чисел
+    for (int i = 0; i < array.length; i++) {
+        st = random.nextBoolean();
+        if (!st) n = (-n);
+        l = (int) (random() * n);
+        array[i] = l;
+    }
+    out.println("Получен массив " + Arrays.toString(array));
+    while ( h <= array.length /3) h = 3 * h + 1;
+    while (h > 0){
+        for (int i = h; i < array.length; i++){
+            int temp = array[i];
+            int j = i;
+            while (j > h - 1 && array[j - h] >= temp){
+                array[j] = array[j - h];
+                j -= h;
+            }
+            array[j] = temp;
+        }
+        h = (h-1) / 3;
+    }
+    out.println("После сортировки получен следующий массив " + Arrays.toString(array));
+    }
+}
+
---
+++ src/com/company/Algorithmization/Task_33/Task33.java
@@ -0,0 +1,62 @@
+package com.company.Algorithmization.Task_33;import java.util.*;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Task33 {
+    public static void main(String[] args) {
+    Random random = new Random();
+    List<Integer> inAr = new ArrayList<>();
+    List<Integer> fromAr = new ArrayList<>();
+    int n, m, array[], array1[], sum[], l, x;
+    boolean st;
+    Scanner sc = new Scanner(in);
+    out.println("Введите размер первого массива ");
+    n = sc.nextInt();
+    array = new int[n];
+    out.println("Введите размер второго массива ");
+    m = sc.nextInt();
+    array1 = new int[m];
+    sum = new int [array1.length+array.length];
+    // Создаём последовательность действительных чисел для первого массива
+    x = array.length;
+    for (int i = 0; i < array.length; i++) {
+        st = random.nextBoolean();
+        if (!st) x = (-x);
+        l = (int) (random() * x);
+        array[i] = l;
+    }
+    Arrays.sort(array);
+    for (int element : array) inAr.add(element);
+    // Создаём последовательность действительных чисел для второго массива
+    x = array1.length;
+    for (int i = 0; i < array1.length; i++) {
+        st = random.nextBoolean();
+        if (!st) x = (-x);
+        l = (int) (random() * x);
+        array1[i] = l;
+    }
+    Arrays.sort(array1);
+    for (int element : array1) fromAr.add(element);
+    List<Integer> indexes = gimmeIndexes(inAr, fromAr);
+    arraycopy(array,0,sum,0,n);
+    arraycopy(array1,0,sum,n,m);
+    Arrays.sort(sum);
+    out.println("Получен первый массив " + Arrays.toString(array));
+    out.println("Получен второй массив " + Arrays.toString(array1));
+    out.println("Куда нужно вставлять, с учетом вставки слева направо " + indexes);
+    out.println("Получен итоговый массив " + Arrays.toString(sum));
+}
+    private static List<Integer> gimmeIndexes(List<Integer> in, List<Integer> from) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < from.size(); i++) {
+            int dex = Collections.binarySearch(in, from.get(i));
+            if (dex < 0) result.add(-(dex + 1) + i);
+            else result.add(dex + i);
+        }
+        return result;
+    }
+}
+
+
+
+

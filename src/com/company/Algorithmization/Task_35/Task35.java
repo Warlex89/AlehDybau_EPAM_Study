fc083d5c534c18365c751a99a3fb90f5353cdd17
Warlex89 <AlehDybau@protonmail.com>
2021-10-30 20:09:35 +0300

    Добавлено задание 35

---
+++ src/com/company/Algorithmization/Task_35/Task35.java
@@ -0,0 +1,30 @@
+package com.company.Algorithmization.Task_35;
+import java.util.Scanner;
+import java.math.BigInteger;
+import static java.lang.System.*;
+
+public class Task35 {
+    public static void main(String[] args){
+        int a, b, nOD, nOK;
+        Task35 tsk = new Task35();
+        Scanner sc = new Scanner(in);
+        out.println("Введите значение переменной а ");
+        a = sc.nextInt();
+        out.println("Введите значение переменной b ");
+        b = sc.nextInt();
+        nOD = tsk.NOD(a,b);
+        nOK = tsk.NOK(a,b,nOD);
+        out.println("Наименьший общий делитель = "+nOD);
+        out.println("Наименьшее общее кратное = "+nOK);
+    }
+    public static int NOD(int a, int b) {
+        BigInteger a1 = BigInteger.valueOf(a);
+        BigInteger b1 = BigInteger.valueOf(b);
+        BigInteger gcd = a1.gcd(b1);
+        return gcd.intValue();
+    }
+    public static int NOK(int a, int b, int nOD){
+        int nOK1 = a * b / nOD;
+        return nOK1;
+    }
+}

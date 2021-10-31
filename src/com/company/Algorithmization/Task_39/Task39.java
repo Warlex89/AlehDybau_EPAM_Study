73d888005f9220b861eddfb37fba19783c403e99
Warlex89 <AlehDybau@protonmail.com>
2021-10-31 21:57:39 +0300

    Добавлено задание 39

---
+++ src/com/company/Algorithmization/Task_39/Task39.java
@@ -0,0 +1,30 @@
+package com.company.Algorithmization.Task_39;
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+public class Task39 {
+    public static void main(String[] args) {
+        int n, array[], l, maxValue = 0;
+        Scanner sc = new Scanner(in);
+        out.println("Укажите размер массива ");
+        n = sc.nextInt();
+        array = new int[n];
+        out.println("Получен следующий массив");
+            for (int i = 0; i < n; i++) {
+                l = (int) (random() * n);
+                array[i] = l;
+                maxValue = (array[i] > maxValue) ? array[i] : maxValue;
+            }
+        out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        for (int i = n-1; i >= 0; i--){
+            if (array[i] < maxValue){
+                out.println("Второе по величине число равно "+array[i]);
+                break;
+            }
+        }
+    }
+}
+

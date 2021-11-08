325c58360eaac02eca3588c070fa6b40d5ea89bc
Warlex89 <AlehDybau@protonmail.com>
2021-11-08 21:54:44 +0300

    Добавлено задание 10

---
+++ src/com/company/String/Task_10/Task10.java
@@ -0,0 +1,23 @@
+package com.company.String.Task_10;
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args){
+        out.println("Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.");
+        String sentence = inputLetter();
+        out.print("В предложении: "+sentence);
+        out.println(" латинская буква “а” встречается: "+countA(sentence)+" раз(а)");
+    }
+    public static String inputLetter (){
+        Scanner sc = new Scanner(in);
+        out.println("Введите слово ");
+        return sc.nextLine();
+    }
+    public static int countA (String sentence){
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++)
+            count = (sentence.charAt(i) == 'a') ? ++count : count;
+        return count;
+    }
+}

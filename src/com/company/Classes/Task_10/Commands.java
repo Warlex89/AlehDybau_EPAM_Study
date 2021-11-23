3ca0a24271f90e420b17aa4ef12763cf49345bad
Warlex89 <AlehDybau@protonmail.com>
2021-11-23 18:20:38 +0300

    Коррективы

--- src/com/company/Classes/Task_10/Commands.java
+++ src/com/company/Classes/Task_10/Commands.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.*;
 
+/*
+ * Создать класс Airline, спецификация которого приведена ниже. Определить конст рукторы, set- и get- методы
+ * и метод  toString(). Создать второй класс, агрегирующий массив типа Airline, с подходящими конструкторами и
+ * методами. Задать критерии выбора данных и вывести эти данные на консоль.
+ *    Airline: пункт назначения, номер рейса, тип самолета, время вылета, дни недели.
+ *    Найти и вывести:
+ *    a) список рейсов для заданного пункта назначения;
+ *    b) список рейсов для заданного дня недели;
+ *    c) список рейсов для заданного дня недели, время вылета для которых больше заданного.
+ */
+
 public class Commands {
     public static void main(String[] args) throws ParseException {
         DateBaseOfAirlines airlines = new DateBaseOfAirlines(new ArrayList<>());

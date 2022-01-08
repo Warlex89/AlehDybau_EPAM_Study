5653ce1e8de9db7571a98870b050d26c6cb22aba
Warlex89 <AlehDybau@protonmail.com>
2022-01-08 15:36:45 +0300

    Отредактировано задание 19

--- src/com/company/Basics/Task_19/Task19.java
+++ src/com/company/Basics/Task_19/Task19.java
@@ -1,6 +1,7 @@
 //Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
 package com.company.Basics.Task_19;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 import static java.lang.System.*;
 
@@ -11,14 +12,21 @@ public class Task19 {
         int a = sc.nextInt();
         out.println("Введите значение числа b ");
         int b = sc.nextInt();
-        out.println("Цифры числа а = "+numbers(a));
-        out.println("Цифры числа а = "+numbers(b));
-
+        ArrayList<Integer> numOfA = numbers(a), numOfB = numbers(b), res = new ArrayList<>();
+        for(Integer num : numOfA){
+            for (Integer integer : numOfB) {
+                if (Objects.equals(num, integer)) {
+                    res.add(num);
+                    break;
+                }
+            }
+        }
+        out.println("Общие цифры для двух чисел: "+res);
     }
-    public static ArrayList numbers(int a){
-        ArrayList<Integer> array = new ArrayList<Integer>();
+    public static ArrayList<Integer> numbers(int a){
+        ArrayList<Integer> array = new ArrayList<>();
+        int i = 0;
         do{
-            int i = 0;
             array.add(i,a%10);
             a = a/10;
             i++;
